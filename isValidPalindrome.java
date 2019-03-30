681a22e3580713720a3c08e0af48b08295607592
bmiche3 <bmiche3@gmail.com>
2019-03-30 14:36:50 -0500

    added isvalidpalindrome

---
+++ isValidPalindrome.java
@@ -0,0 +1,22 @@
+//"A man, a plan, a canal: Panama" should return true
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length()-1;
+        while(i<j){
+            while(!Character.isLetterOrDigit(s.charAt(i)) && i<j){
+                i++;
+            }
+            while(!Character.isLetterOrDigit(s.charAt(j)) && i<j){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
